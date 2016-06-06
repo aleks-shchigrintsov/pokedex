1b5c9b4868284007bedf8650b0ec218da185bdd8
Alex S <aleksrullz@gmail.com>
2016-06-06 23:09:23 +0300

    layout of main VC

---
+++ pokedex1/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  pokedex1
+//
+//  Created by Aleksandr Vladimirovich on 06.06.16.
+//  Copyright © 2016 Aleksandr Vladimirovich. All rights reserved.
+//
+
+import Foundation
