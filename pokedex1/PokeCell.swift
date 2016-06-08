5fe3fac8291d71d97d4d57e2709b3f2c92812cd1
Alex S <aleksrullz@gmail.com>
2016-06-08 23:23:37 +0300

    added PokeCell VIEW

---
+++ pokedex1/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex1
+//
+//  Created by Aleksandr Vladimirovich on 08.06.16.
+//  Copyright © 2016 Aleksandr Vladimirovich. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
